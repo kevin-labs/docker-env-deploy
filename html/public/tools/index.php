812defd7357e5cba12aed9a164cb77743f97b8b5
youe1 <you@example.com>
2023-05-22 20:33:47 +0800

    v2.1.1 更新文档；添加了install-sh；添加restic

--- html/public/tools/index.php
+++ html/public/tools/index.php
@@ -27,7 +27,7 @@
                     </div>
                 </div>
             </div>
-            <div class="col-6 col-md-4">
+            <div class="col-6 col-md-3">
                 <div class="card mb-2 border-secondary">
                     <div class="card-body">
                         <h5 class="card-title">File Manger  <span class="badge bg-danger">File</span> </h5>
@@ -36,7 +36,7 @@
                     </div>
                 </div>
             </div>
-            <div class="col-6 col-md-4">
+            <div class="col-6 col-md-3">
                 <div class="card mb-2 border-secondary">
                     <div class="card-body">
                         <h5 class="card-title">SQL Manager <span class="badge bg-success">SQL</span> </h5>
@@ -46,12 +46,21 @@
                     </div>
                 </div>
             </div>
-            <div class="col-6 col-md-4">
+            <div class="col-6 col-md-3">
                 <div class="card mb-2 border-secondary">
                     <div class="card-body">
                         <h5 class="card-title">PHP Info <span class="badge bg-success">Info</span> </h5>
                         <p class="card-text">PHP System Info.</p>
                         <a href="/phpinfo.php" target="_blank" class="btn btn-sm btn-outline-dark mb-1">PHPINFO</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-6 col-md-3">
+                <div class="card mb-2 border-secondary">
+                    <div class="card-body">
+                        <h5 class="card-title">Code-Serve <span class="badge bg-success">Info</span> </h5>
+                        <p class="card-text">Code Serve.</p>
+                        <!-- <a href="/phpinfo.php" target="_blank" class="btn btn-sm btn-outline-dark mb-1">Code Serve</a> -->
                     </div>
                 </div>
             </div>
