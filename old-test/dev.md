9d37ecad86a6bd173ac2defba25308cb3eaacda5
youe1 <you@example.com>
2023-06-17 09:20:07 +0800

    del old

--- old-test/dev.md
+++
@@ -1,8 +0,0 @@
-
-## 下个版本
-
-- Docker 快速安装
-- 管理工具，远程加载配置和权限设置~
-- docker-compose 的管理~
-- code-server 内存消耗，轻量替代~
-- restic 自动备份
--- old-test/html/public/tools/filemanager/lib.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$auth_users = [
-    'admin' => '$2y$10$EuGiI/egH6zF9Pr2lCbtH.nK0iBWu.kEKd5NxxGP.tzVE8QB5TsW6', 
-];
-
-$use_auth = true;
